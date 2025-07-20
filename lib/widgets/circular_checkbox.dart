a60d234e9bf76908f46059b279247d5c35d75c31
Phil Bogle <phil.bogle@databricks.com>
2025-07-20 12:06:59 -0700

    Add file

---
+++ lib/widgets/circular_checkbox.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+class CircularCheckbox extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool?>? onChanged;
+
+  const CircularCheckbox({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        onChanged?.call(!value);
+      },
+      customBorder: const CircleBorder(),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: value ? Theme.of(context).primaryColor : Colors.grey,
+            width: 2.0,
+          ),
+          color: value ? Theme.of(context).primaryColor : Colors.transparent,
+        ),
+        child: value
+            ? const Icon(
+                Icons.check,
+                size: 20.0,
+                color: Colors.white,
+              )
+            : const SizedBox(
+                width: 20.0,
+                height: 20.0,
+              ),
+      ),
+    );
+  }
+}
