c834595f3cd076307a1977e5639e270ee40fc9bf
Phil Bogle <philbogle@gmail.com>
2025-07-23 22:32:56 -0700

    animation effect when marking list complete

--- lib/widgets/list_card.dart
+++ lib/widgets/list_card.dart
@@ -16,13 +16,29 @@ class ListCard extends StatefulWidget {
   State<ListCard> createState() => _ListCardState();
 }
 
-class _ListCardState extends State<ListCard> {
+class _ListCardState extends State<ListCard> with SingleTickerProviderStateMixin {
   late bool _optimisticCompleted;
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     _optimisticCompleted = widget.list.completed;
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -40,60 +56,72 @@ class _ListCardState extends State<ListCard> {
       _optimisticCompleted = value;
     });
 
-    widget.onCompleted(value);
+    if (value) {
+      _animationController.forward().then((_) {
+        widget.onCompleted(value);
+      });
+    } else {
+      widget.onCompleted(value);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) =>
-                  ListDetailScreen(listId: widget.list.id),
-              transitionsBuilder:
-                  (context, animation, secondaryAnimation, child) {
-                return FadeTransition(
-                  opacity: animation,
-                  child: child,
-                );
-              },
-            ),
-          );
-        },
-        child: Column(
-          children: [
-            ListTile(
-              leading: CircularCheckbox(
-                value: _optimisticCompleted,
-                onChanged: _handleCheckboxChanged,
-              ),
-              title: Text(
-                widget.list.title,
-                style: TextStyle(
-                  decoration: _optimisticCompleted
-                      ? TextDecoration.lineThrough
-                      : null,
-                  color: _optimisticCompleted ? Colors.grey : null,
-                  fontWeight: FontWeight.bold,
+    return FadeTransition(
+      opacity: _animation.drive(Tween(begin: 1.0, end: 0.0)),
+      child: SizeTransition(
+        sizeFactor: _animation.drive(Tween(begin: 1.0, end: 0.0)),
+        child: Card(
+          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      ListDetailScreen(listId: widget.list.id),
+                  transitionsBuilder:
+                      (context, animation, secondaryAnimation, child) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
                 ),
-              ),
-              trailing: const Icon(Icons.chevron_right),
-            ),
-            ...widget.list.subitems
-                .where((s) => s.title.isNotEmpty)
-                .map((subitem) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 32.0),
-                child: ReadOnlySubtaskItem(
-                    subitem: subitem, listId: widget.list.id),
               );
-            }).toList(),
-            const SizedBox(height: 8), // Add some padding at the bottom
-          ],
+            },
+            child: Column(
+              children: [
+                ListTile(
+                  leading: CircularCheckbox(
+                    value: _optimisticCompleted,
+                    onChanged: _handleCheckboxChanged,
+                  ),
+                  title: Text(
+                    widget.list.title,
+                    style: TextStyle(
+                      decoration: _optimisticCompleted
+                          ? TextDecoration.lineThrough
+                          : null,
+                      color: _optimisticCompleted ? Colors.grey : null,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: const Icon(Icons.chevron_right),
+                ),
+                ...widget.list.subitems
+                    .where((s) => s.title.isNotEmpty)
+                    .map((subitem) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 32.0),
+                    child: ReadOnlySubtaskItem(
+                        subitem: subitem, listId: widget.list.id),
+                  );
+                }).toList(),
+                const SizedBox(height: 8), // Add some padding at the bottom
+              ],
+            ),
+          ),
         ),
       ),
     );
